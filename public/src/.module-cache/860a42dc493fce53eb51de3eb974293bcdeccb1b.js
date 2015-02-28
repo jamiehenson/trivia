c4aeee102b6797f353fb84b319c6f923cb409d02
Jamie Henson <jhenson47@gmail.com>
2015-02-28 13:53:26 -0800

    React and node baseplate

---
+++ public/src/.module-cache/860a42dc493fce53eb51de3eb974293bcdeccb1b.js
@@ -0,0 +1,53 @@
+var converter = new Showdown.converter();
+
+var CommentBox = React.createClass({displayName: "CommentBox",
+  render: function() {
+    return (
+      React.createElement("div", {className: "commentBox"}, 
+        React.createElement("h1", null, "Comments"), 
+        React.createElement(CommentList, null), 
+        React.createElement(CommentForm, null)
+      )
+    );
+  }
+});
+
+var CommentList = React.createClass({displayName: "CommentList",
+  render: function() {
+    return (
+      React.createElement("div", {className: "commentList"}, 
+        React.createElement(Comment, {author: "Toadfish"}, "\"I'm gonna ruin your life\""), 
+        React.createElement(Comment, {author: "Stonefish"}, "\"I'm youre worst fucking nightmare if you don't like toadfish\"")
+      )
+    );
+  }
+});
+
+var CommentForm = React.createClass({displayName: "CommentForm",
+  render: function() {
+    return (
+      React.createElement("div", {className: "commentForm"}, 
+        "Hello, world! I am a CommentForm."
+      )
+    );
+  }
+});
+
+var Comment = React.createClass({displayName: "Comment",
+  render: function() {
+    var rawMarkup = converter.makeHtml(this.props.children.toString());
+    return (
+      React.createElement("div", {className: "comment"}, 
+        React.createElement("h2", {className: "commentAuthor"}, 
+          this.props.author
+        ), 
+        React.createElement("span", {dangerouslySetInnerHTML: {__html: rawMarkup}})
+      )
+    );
+  }
+});
+
+React.render(
+  React.createElement(CommentBox, null),
+  document.getElementById('content')
+);
---
+++ public/src/.module-cache/f490c97279835a91bb2f8ccc66807e367657a571.js
@@ -0,0 +1,64 @@
+var converter = new Showdown.converter();
+
+var CommentBox = React.createClass({displayName: "CommentBox",
+  render: function() {
+    return (
+      React.createElement("div", {className: "commentBox"}, 
+        React.createElement("h1", null, "Comments"), 
+        React.createElement(CommentList, {data: this.props.data}), 
+        React.createElement(CommentForm, null)
+      )
+    );
+  }
+});
+
+var CommentList = React.createClass({displayName: "CommentList",
+  render: function() {
+    var commentNodes = this.props.data.map(function (comment) {
+      return (
+        React.createElement(Comment, {author: comment.author}, 
+          comment.text
+        )
+      );
+    });
+    return (
+      React.createElement("div", {className: "commentList"}, 
+        commentNodes
+      )
+    );
+  }
+});
+
+var CommentForm = React.createClass({displayName: "CommentForm",
+  render: function() {
+    return (
+      React.createElement("div", {className: "commentForm"}, 
+        "Hello, world! I am a CommentForm."
+      )
+    );
+  }
+});
+
+var Comment = React.createClass({displayName: "Comment",
+  render: function() {
+    var rawMarkup = converter.makeHtml(this.props.children.toString());
+    return (
+      React.createElement("div", {className: "comment"}, 
+        React.createElement("h2", {className: "commentAuthor"}, 
+          this.props.author
+        ), 
+        React.createElement("span", {dangerouslySetInnerHTML: {__html: rawMarkup}})
+      )
+    );
+  }
+});
+
+var data = [
+  {author: "Toadfish", text: "I'm gonna ruin your life"},
+  {author: "Stonefish", text: "I'm youre worst fucking nightmare if you don't like toadfish"}
+];
+
+React.render(
+  React.createElement(CommentBox, {data: data}),
+  document.getElementById('content')
+);
